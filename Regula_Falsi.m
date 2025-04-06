919bc2669a86bdd31f62414bb5da4b5798d3daed
Jose <joseaguilarsalazar2004@gmail.com>
2025-04-06 17:24:44 -0500

    ejercicio 02 agregado

--- Regula_Falsi.m
+++ Regula_Falsi.m
@@ -2,17 +2,12 @@ F = input('Ingrese la ecuación: ', 's');
 a = input('Primer valor del intervalo: ');
 b = input('Segundo valor del intervalo: ');
 margen = 0.00000001;
-ecuacion = x^2;
 
-funcionx(x){
-    resultado = x^2;
-    return resultado;
-}
 % Convertir string a función anónima
 f = str2func(['@(x) ', F]);
 while true
     fprintf('[%.6f, %.6f]\n', a, b);
-    c = (a + b)/2;
+    c = ((f(b)*a)-(f(a)*b))/(f(b)-f(a));
     if f(c) == 0 || abs(f(c)) < margen
         fprintf('La raíz es: %.6f\n', c);
         fprintf('El valor de f(c) es: %.6f\n', f(c));
