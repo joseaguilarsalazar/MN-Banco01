6e2f519bc9bd06e3f39ae323a7405500735f9143
Anny Celeste Peñaherrera Rengifo <2225254@unapiquitos.edu.pe>
2025-05-12 02:46:57 -0500

    Actualización de raiz emitiendo lineas

--- raiz.m
+++ raiz.m
@@ -1,35 +1,38 @@
+clc; clear;
+
+% Entrada del número
 n = input('Ingrese el número del cual desea calcular la raíz cuadrada: ');
 
-% Definir automáticamente el intervalo
+% Verificación de número positivo
+if n <= 0
+    error('Debe ingresar un número positivo mayor que cero.');
+end
+
+% Definir intervalo [a, b]
 if n >= 1
     a = 0;
     b = n;
-elseif n > 0 && n < 1
+else
     a = 0;
     b = 1;
-else
-    error('Debe ingresar un número positivo.');
 end
 
+% Precisión
 margen = 1e-8;
 
-% Definir la función
+% Definir función f(x)
 f = @(x) x.^2 - n;
 
-% Verificar que haya cambio de signo (en teoría siempre lo habrá con este intervalo)
-if f(a) * f(b) > 0
-    error('La función no cambia de signo en el intervalo. No se puede aplicar bisección.');
-end
-
+% Método de bisección
 while true
-    fprintf('[%.8f, %.8f]\n', a, b);
     c = (a + b) / 2;
-    if f(c) == 0 || abs(f(c)) < margen
-        fprintf('La raíz aproximada es: %.8f\n', c);
-        fprintf('El valor de f(c) es: %.8f\n', f(c));
+
+    if abs(f(c)) < margen
+        fprintf('Su raiz aproximada es: %.8f\n', c);  % Solo imprime la raíz
         break
     end
-    if sign(f(c)) == sign(f(a))
+
+    if f(a) * f(c) > 0
         a = c;
     else
         b = c;
